ef687ae805eaf6316e062fb56cd4d966f59f7a56
Raghav Subramaniam <raghavs511@gmail.com>
2017-11-06 14:04:34 -0800

    Update version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='recolor_dots',
-      version='0.1.2',
+      version='0.1.3',
       description='Templates for dotfile recoloring',
       url='http://github.com/raghavsub/recolor-dots',
       author='Raghav Subramaniam',
